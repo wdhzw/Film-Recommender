0e6b7868150ba53d015a04c783a26086b000afe5
Aatrox <guohaonan980421@163.com>
2024-04-19 11:12:06 +0800

    [feat]recommend

--- recommendation_server/internal/recommendation/usecase/recommendation_usecase.go
+++ recommendation_server/internal/recommendation/usecase/recommendation_usecase.go
@@ -1,64 +1,121 @@
 package usecase
 
 import (
-    "fmt"
-    "strings"
+	"fmt"
+	"sort"
+	"strings"
 
-    "recommendation_server/internal/recommendation/infra"
+	"recommendation_server/internal/recommendation/infra"
 )
 
 type RecommendationUsecase struct {
-    userApiClient   *infra.UserApiClient
-    movieApiClient  *infra.MovieApiClient
+	userApiClient  *infra.UserApiClient
+	movieApiClient *infra.MovieApiClient
 }
 
 func NewRecommendationUsecase(userApiClient *infra.UserApiClient, movieApiClient *infra.MovieApiClient) *RecommendationUsecase {
-    return &RecommendationUsecase{
-        userApiClient:   userApiClient,
-        movieApiClient:  movieApiClient,
-    }
+	return &RecommendationUsecase{
+		userApiClient:  userApiClient,
+		movieApiClient: movieApiClient,
+	}
+}
+
+type Item struct {
+	Movie infra.MovieModel
+	Score float64
 }
 
 func (uc *RecommendationUsecase) GeneratePersonalizedRecommendations(email string) ([]infra.MovieModel, error) {
-    // Fetch user data
-    user, err := uc.userApiClient.FetchUserByEmail(email)
-    if err != nil {
-        return nil, fmt.Errorf("error fetching user data: %v", err)
-    }
+	// Fetch user data
+	user, err := uc.userApiClient.FetchUserByEmail(email)
+	if err != nil {
+		return nil, fmt.Errorf("error fetching user data: %v", err)
+	}
 
-    // Fetch popular and high-rated movies
-    popularMovies, err := uc.movieApiClient.GetPopularMovies()
-    if err != nil {
-        return nil, fmt.Errorf("error fetching popular movies: %v", err)
-    }
-    highRateMovies, err := uc.movieApiClient.GetHighRateMovies()
-    if err != nil {
-        return nil, fmt.Errorf("error fetching high-rate movies: %v", err)
-    }
+	// Fetch popular and high-rated movies
+	popularMovies, err := uc.movieApiClient.GetPopularMovies()
+	if err != nil {
+		return nil, fmt.Errorf("error fetching popular movies: %v", err)
+	}
+	highRateMovies, err := uc.movieApiClient.GetHighRateMovies()
+	if err != nil {
+		return nil, fmt.Errorf("error fetching high-rate movies: %v", err)
+	}
 
-    // Combine popular and high-rated movies
-    allMovies := append(popularMovies, highRateMovies...)
+	preferences := user.PreferredGenre
+	preferenceSet := make(map[string]struct{}, 0)
+	for _, p := range preferences {
+		if _, ok := preferenceSet[p]; !ok {
+			preferenceSet[p] = struct{}{}
+		}
+	}
 
-    // Filter movies based on user's preferred genres
-    var recommendedMovies []infra.MovieModel
-    for _, movie := range allMovies {
-        movieGenres := strings.Split(movie.Genres, ",")
-        for _, genre := range user.PreferredGenre {
-            if containsGenre(movieGenres, genre) {
-                recommendedMovies = append(recommendedMovies, movie)
-                break
-            }
-        }
-    }
+	movies := make(map[int64]*Item, 0)
+	for _, popularMovie := range popularMovies {
+		mid := popularMovie.MovieID
 
-    return recommendedMovies, nil
+		if _, ok := movies[mid]; !ok {
+			movies[mid] = &Item{
+				Movie: popularMovie,
+				Score: popularMovie.Popularity,
+			}
+		}
+
+		movieGenre := popularMovie.Genres
+		movieGenres := strings.Split(movieGenre, ",")
+
+		for _, g := range movieGenres {
+			if _, ok := preferenceSet[g]; ok {
+				movies[mid].Score += 5
+			}
+		}
+	}
+
+	for _, rateMovie := range highRateMovies {
+		mid := rateMovie.MovieID
+
+		if _, ok := movies[mid]; !ok {
+			movies[mid] = &Item{
+				Movie: rateMovie,
+				Score: rateMovie.Rate,
+			}
+		} else {
+			movies[mid].Score += rateMovie.Rate
+		}
+
+		movieGenre := rateMovie.Genres
+		movieGenres := strings.Split(movieGenre, ",")
+
+		for _, g := range movieGenres {
+			if _, ok := preferenceSet[g]; ok {
+				movies[mid].Score += 5
+			}
+		}
+	}
+
+	rawItems := make([]Item, 0, len(movies))
+	for _, m := range movies {
+		rawItems = append(rawItems, m)
+	}
+	//  sort
+	sort.Slice(rawItems, func(i, j int) bool {
+		return rawItems[i].Score < rawItems[j].Score
+	})
+
+	results := make([]infra.MovieModel, 0, len(rawItems))
+
+	for _, i := range rawItems {
+		results = append(results, i.Movie)
+	}
+
+	return results, nil
 }
 
 func containsGenre(genres []string, target string) bool {
-    for _, genre := range genres {
-        if strings.TrimSpace(genre) == target {
-            return true
-        }
-    }
-    return false
+	for _, genre := range genres {
+		if strings.TrimSpace(genre) == target {
+			return true
+		}
+	}
+	return false
 }
